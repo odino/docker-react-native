76790a6e3656448c265d796a0febe2c407abf9b6
odino <alessandro.nadalin@gmail.com>
2017-10-07 21:43:07 +0400

    some dumb stuff

--- react-native-container.sh
+++ react-native-container.sh
@@ -22,8 +22,9 @@ fi
 docker run \
        --rm \
        -it \
-       --user="$UID" \
+       --user="root" \
        --privileged \
+       -v /home/$USER/.bash_history:/root/.bash_history \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /dev/bus/usb:/dev/bus/usb \
        -v /home/$USER/.android:/home/dev/.android \
